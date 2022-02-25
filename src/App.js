433bd52f5d2ccb2b3a47f9fc1c4c44f96cbaa0aa
justin tubay <justin.tubay@gmail.com>
2022-02-25 00:38:07 -0500

    fixed compile warnings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <img 
+        alt="God Sage"
         height="100%"
         width="100%"
         src="https://c.tenor.com/lSflRMQECFYAAAAd/sage-valorant.gif"></img>
