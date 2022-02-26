3912fddf5a67d1e0e7fee8352b63d4ca7f4e8c86
justin tubay <justin.tubay@gmail.com>
2022-02-26 11:57:26 -0500

    god

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <img 
-        alt="God Sage"
-        height="100%"
-        width="100%"
-        src="https://c.tenor.com/lSflRMQECFYAAAAd/sage-valorant.gif"></img>
+     Hi
     </div>
   );
 }
